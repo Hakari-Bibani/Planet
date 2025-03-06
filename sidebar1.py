1113d4a7501ac49d6f1b006b6208b0e447937dca
Hakari Bibani <hakary.jalal33@gmail.com>
2025-03-06 18:09:14 +0300

    Update sidebar1.py

--- sidebar1.py
+++ sidebar1.py
@@ -6,6 +6,8 @@ def sidebar_menu():
         st.session_state.selected_page = "Home"
     if st.sidebar.button("Home"):
         st.session_state.selected_page = "Home"
+        st.session_state.purchase_mode = False  # ensure purchase mode is off
     if st.sidebar.button("Status"):
         st.session_state.selected_page = "Status"
+        st.session_state.purchase_mode = False
     return st.session_state.selected_page
