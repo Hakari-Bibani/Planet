46b5fd54f9250c769f58992031758bc29ef23529
Hakari Bibani <hakary.jalal33@gmail.com>
2025-03-06 18:05:00 +0300

    Update sidebar1.py

--- sidebar1.py
+++ sidebar1.py
@@ -1,15 +1,11 @@
 import streamlit as st
 
 def sidebar_menu():
+    st.sidebar.markdown("<h2>Menu</h2>", unsafe_allow_html=True)
     if 'selected_page' not in st.session_state:
-        st.session_state.selected_page = "Search"
-
-    st.sidebar.title("Menu")
-    if st.sidebar.button("Search"):
-        st.session_state.selected_page = "Search"
-    if st.sidebar.button("Purchase"):
-        st.session_state.selected_page = "Purchase"
+        st.session_state.selected_page = "Home"
+    if st.sidebar.button("Home"):
+        st.session_state.selected_page = "Home"
     if st.sidebar.button("Status"):
         st.session_state.selected_page = "Status"
-
     return st.session_state.selected_page
