d3a27b927d25aded5bb69f9b99b6b26b7f62a4a1
Hakari Bibani <hakary.jalal33@gmail.com>
2025-03-07 05:20:18 +0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -3,9 +3,6 @@ import streamlit as st
 if "purchase_mode" not in st.session_state:
     st.session_state.purchase_mode = False
 
-# Debug print to view current session state.
-st.write("DEBUG: Session State in app1.py", st.session_state)
-
 if st.session_state.purchase_mode:
     import purchase
     purchase.purchase_page()
